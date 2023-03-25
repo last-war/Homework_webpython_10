4e18bf96abb92d0688e5ec743f3ffc96c3c83101
Андрій <andrey.maybrodskyy@gmail.com>
2023-03-25 16:54:49 +0200

    fin of f. jango!

--- Bestquotes/quotesapp/forms.py
+++ Bestquotes/quotesapp/forms.py
@@ -1,5 +1,16 @@
-from django.forms import ModelForm, CharField, TextInput
+from django.forms import ModelForm, CharField, TextInput, DateField, SelectDateWidget
 from .models import Tag, Author, Quote
+
+
+class AuthorForm(ModelForm):
+    fullname = CharField(max_length=150, required=True, widget=TextInput())
+    born_date = DateField(widget=SelectDateWidget(years=[i for i in range(1800, 2023)]))
+    born_location = CharField(max_length=150, widget=TextInput())
+    description = CharField(max_length=1000, widget=TextInput())
+
+    class Meta:
+        model = Author
+        fields = ['fullname', 'born_date', 'born_location', 'description']
 
 
 class TagForm(ModelForm):
@@ -13,9 +24,9 @@ class TagForm(ModelForm):
 class QuoteForm(ModelForm):
 
     quote = CharField(min_length=5, max_length=500, required=True, widget=TextInput())
-    author = CharField(min_length=10, max_length=150, required=True, widget=TextInput())
+    author = CharField(min_length=1, max_length=150, widget=TextInput())
 
     class Meta:
         model = Quote
-        fields = ['quote', 'author']
-        exclude = ['tags']
+        fields = ['quote']
+        exclude = ['tags', 'author']
--- Bestquotes/quotesapp/urls.py
+++ Bestquotes/quotesapp/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('', views.main, name='main'),
     path('quote/', views.quote, name='quote'),
     path('tag/', views.tag, name='tag'),
+    path('author/', views.author, name='author'),
+    path('authors/', views.authors, name='authors'),
+    path('detail/<int:author_id>', views.detail, name='detail'),
+    path('fill_db/', views.fill_db, name='fill_db'),
 ]
